040ee3e0c9b95eb88a484d7759724d702052ca6c
romulus-lanceues <lanceabuela@gmail.com>
2026-07-24 17:52:30 +0800

    feat: added a custom jackson deserializer to turn submissions that only contains space to null

--- src/main/java/com/lancea/personal_finance_loan_api/dto/request/UpdateInfoRequest.java
+++ src/main/java/com/lancea/personal_finance_loan_api/dto/request/UpdateInfoRequest.java
@@ -2,12 +2,14 @@ package com.lancea.personal_finance_loan_api.dto.request;
 
 import com.lancea.personal_finance_loan_api.validation.annotation.AtLeastOneField;
 import jakarta.validation.constraints.Email;
+import tools.jackson.databind.annotation.JsonDeserialize;
 
 @AtLeastOneField
 public record UpdateInfoRequest(
-        
+        @JsonDeserialize(using = BlankToNullStringDeserializer.class)
         String fullName,
 
         @Email
+        @JsonDeserialize(using = BlankToNullStringDeserializer.class)
         String email) {
 }
